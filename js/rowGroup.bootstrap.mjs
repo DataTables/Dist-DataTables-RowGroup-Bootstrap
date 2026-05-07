eb4a0eb4eea55fed1a0007e76443c3577d7e30b2
dtbuild <dtbuild@rigil>
2026-05-07 13:51:27 +0000

    8183b71a55322d9cf9245633002da19c0b7e9b12
    Sync to source repo @1d80efc25e05b80d202aa5699e2950417ecaf4ef

--- js/rowGroup.bootstrap.mjs
+++ js/rowGroup.bootstrap.mjs
@@ -1,14 +1,11 @@
-/*! Bootstrap 3 styling wrapper for RowGroup
- * © SpryMedia Ltd - datatables.net/license
+/*! RowGroup Bootstrap 3 styling 2.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
-import jQuery from 'jquery';
 import DataTable from 'datatables.net-bs';
 import RowGroup from 'datatables.net-rowgroup';
-
-// Allow reassignment of the $ variable
-let $ = jQuery;
 
 
 
 export default DataTable;
+
